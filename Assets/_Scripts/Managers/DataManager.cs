86c51206a647b632cfcb4f98436b53fc4999aba0
Neunggwon <104968524+Neunggwon@users.noreply.github.com>
2024-08-25 21:34:12 +0900

    [Add] Scripts
    DataManager
    ResourceManager

---
+++ Assets/_Scripts/Managers/DataManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    //날짜, 플레이어 위치, 인벤토리, 자연환경, 등
+
+    public void LoudData()
+    {
+
+    }
+
+    public void SaveData()
+    {
+
+    }
+}
