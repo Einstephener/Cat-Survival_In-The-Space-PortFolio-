7db7c0084f0ac3765a7bb4828d719126e3f851b9
Einstephener <kihyuk0201@gmail.com>
2024-08-26 14:51:42 +0900

    [Feat] UIManager SetCanvas
    [Feat] UIManager에 캔버스 설정 기능
    [Add] .editorconfig로 이제부터 만드는 scripts들은 한글 주석을 inspector창에서 확인할 수 있음.
    [Add] UI_Base 스크립트 생성

---
+++ Assets/_Scripts/UI/UI_Base.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Base : MonoBehaviour
+{
+    // UI POPUP, UI Scene의 부모가 될 스크립트
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
