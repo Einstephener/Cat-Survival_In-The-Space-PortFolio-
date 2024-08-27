b2c3c6a1a61b827dd84f7bab6d15dce27e0a4193
Jya0w0 <jya0w0@gmail.com>
2024-08-27 17:10:10 +0900

    [Feat] Input System

---
+++ Assets/_Scripts/Creature/Player/Control/PlayerInputController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    [SerializeField] private Vector2 _moveInput;
+    [SerializeField] private float _speed;
+
+    private Rigidbody _rigid;
+
+    private void Awake()
+    {
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 nextVec = _moveInput * _speed * Time.fixedTime;
+        _rigid.MovePosition(_rigid.position + nextVec);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        //Debug.Log("OnMove" + value.ToString());
+        _moveInput = value.Get<Vector2>();
+    }
+
+    private void OnLook(InputValue value)
+    {
+        Debug.Log("OnLook" + value.ToString());
+    }
+
+    private void OnFire(InputValue value)
+    {
+        Debug.Log("OnFire" + value.ToString());
+    }
+}
