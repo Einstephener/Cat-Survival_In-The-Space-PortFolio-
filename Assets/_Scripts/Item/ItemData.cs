8bb28eeb52bc7eadfbd7ba5a53382b671365d1ce
Einstephener <kihyuk0201@gmail.com>
2024-08-31 14:15:05 +0900

    [Add] ItemData
    아이템 종류, 데이터를 담은 ItemData (S.O.)파일 추가

---
+++ Assets/_Scripts/Item/ItemData.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Resource,
+    Equipable,
+    Consumable
+}
+
+public enum ConsumableType
+{
+    Hunger,
+    Health,
+    Thirsty
+}
+
+public enum EquipableType
+{
+    Ax,
+    Pick,
+    Weapon
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+public class ItemDataEquipable
+{
+    public EquipableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string DisplayName;
+    public string Description;
+    public ItemType Type;
+    public Sprite Icon;
+    public GameObject DropPrefab;
+
+    [Header("Stacking")]
+    public bool CanStack;
+    public int MaxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] Consumables;
+
+    [Header("Equip")]
+    public GameObject EquipPrefab;
+
+    [Header("Equipable")]
+    public ItemDataEquipable[] Equipables;
+}
