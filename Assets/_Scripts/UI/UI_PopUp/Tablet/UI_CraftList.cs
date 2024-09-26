6d89900ca318b1f7d964b90490ab1be860199904
Einstephener <kihyuk0201@gmail.com>
2024-09-26 17:11:48 +0900

    [Fix] Item Craft, UpdateResourceAmount
    아이템 제작 등으로 인해 아이템 개수의 변동이 있을 때의 코드.

--- Assets/_Scripts/UI/UI_PopUp/Tablet/UI_CraftList.cs
+++ Assets/_Scripts/UI/UI_PopUp/Tablet/UI_CraftList.cs
@@ -12,57 +12,91 @@ public class UI_CraftList : MonoBehaviour
     public Button CraftBTN;
     private ItemData _itemData;
 
-    private Image ResourceItemImage;
-    private TMP_Text ResourceItemAmount;
-    private int _itemOwnedAmount = 0; //todo
-    private int _itemNeedAmount;
-
+    private List<TMP_Text> _resourceTexts = new(); // 각 재료의 수량 텍스트를 저장.
+    private List<int> _resourceOwnedAmounts = new();    // 각 재료의 현재 소유 개수 저장.
 
     public void ClickCraftBTN()
     {
+        // 아이템 제작을 처리하는 메서드.
+        if (_itemData.DropPrefab != null)
+        {
+            // 아이템을 소환 (플레이어 좌표로 수정 필요).
+            Instantiate(_itemData.DropPrefab, Vector3.zero, Quaternion.identity);
 
+            // 재료 차감.
+            for (int i = 0; i < _itemData.CraftingResourceList.Length; i++)
+            {
+                Main.Inventory.RemoveItem(_itemData.CraftingResourceList[i].ResourceData, _itemData.CraftingResourceList[i].Amount);
+
+                // 재료의 남은 수량 업데이트.
+                _resourceOwnedAmounts[i] = Main.Inventory.GetTotalItemCount(_itemData.CraftingResourceList[i].ResourceData);
+            }
+
+            // UI 업데이트.
+            UpdateResourceTexts();
+        }
     }
 
     public void CanCraft()
     {
-        if(_itemOwnedAmount < _itemNeedAmount)
+        // 아이템을 제작할 수 있는지 확인하고, 버튼 활성화 여부 결정.
+        bool canCraft = true;
+
+        for (int i = 0; i < _itemData.CraftingResourceList.Length; i++)
         {
-            // 제작 불가
-            CraftBTN.interactable = false;
+            if (_resourceOwnedAmounts[i] < _itemData.CraftingResourceList[i].Amount)
+            {
+                canCraft = false;
+                break;
+            }
         }
-        else
-        {
-            //제작 가능
-            CraftBTN.interactable = true;
-        }
+
+        CraftBTN.interactable = canCraft;
     }
 
     public void InitSetting(GameObject _craftItem)
     {
+        // 아이템 데이터를 초기화하고 UI에 반영하는 메서드.
         if (_craftItem.TryGetComponent(out ItemObject _itemObject))
         {
             _itemData = _itemObject.itemData;
             CraftItemImage.sprite = _itemData.Icon;
-            for(int i = 0; i < _itemData.CraftingResourceList.Length; i++)
+
+            for (int i = 0; i < _itemData.CraftingResourceList.Length; i++)
             {
-                GameObject ResourceIcon = Instantiate(CraftItemResoursePrefab, CraftDescriptionParent.transform);
+                // 재료 아이콘 및 수량 표시.
+                GameObject resourceIcon = Instantiate(CraftItemResoursePrefab, CraftDescriptionParent.transform);
+                resourceIcon.GetComponent<Image>().sprite = _itemData.CraftingResourceList[i].ResourceData.Icon;
 
-                ResourceIcon.GetComponent<Image>().sprite = _itemData.CraftingResourceList[i].ResourceData.Icon;
+                int ownedAmount = Main.Inventory.GetTotalItemCount(_itemData.CraftingResourceList[i].ResourceData);
+                int needAmount = _itemData.CraftingResourceList[i].Amount;
 
-                _itemOwnedAmount = Main.Inventory.GetTotalItemCount(_itemData.CraftingResourceList[i].ResourceData);
-                _itemNeedAmount = _itemData.CraftingResourceList[i].Amount;
-                CanCraft();
+                // 재료의 현재 소유 수량과 필요 수량 저장.
+                _resourceOwnedAmounts.Add(ownedAmount);
 
-                ResourceIcon.GetComponentInChildren<TMP_Text>().text = _itemOwnedAmount + " / " + _itemNeedAmount;
-
-
+                TMP_Text resourceText = resourceIcon.GetComponentInChildren<TMP_Text>();
+                resourceText.text = ownedAmount + " / " + needAmount;
+                _resourceTexts.Add(resourceText);
             }
+
+            CanCraft();
         }
         else
         {
-            Debug.LogError("ItemObject");
+            Debug.LogError("ItemObject is missing.");
         }
     }
 
+    private void UpdateResourceTexts()
+    {
+        // 남은 재료 수량을 다시 UI에 업데이트하는 메서드.
+        for (int i = 0; i < _itemData.CraftingResourceList.Length; i++)
+        {
+            int needAmount = _itemData.CraftingResourceList[i].Amount;
+            _resourceTexts[i].text = _resourceOwnedAmounts[i] + " / " + needAmount;
+        }
 
+        // 버튼 상태도 재확인.
+        CanCraft();
+    }
 }
