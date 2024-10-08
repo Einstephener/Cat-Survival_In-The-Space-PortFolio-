8483b53dc691de9d363d45073bb4d8d7ce4c76f5
Einstephener <kihyuk0201@gmail.com>
2024-10-08 13:34:40 +0900

    [Fix] sit sight soft
    앉았을 때 시야 변화를 부드럽게 변경

--- Assets/_Scripts/Creature/Player/Condition/Control/CameraController.cs
+++ Assets/_Scripts/Creature/Player/Condition/Control/CameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Coroutine _crouchCoroutine; // 코루틴 핸들
     private float _sitSight = .8f;
     private float _idleSight = 1.7f;
     [SerializeField] GameObject PlayerHead;
@@ -13,11 +14,37 @@ public class CameraController : MonoBehaviour
         // 실제 카메라의 Quaternion 회전에 적용.
         transform.localRotation = Quaternion.Euler(eulerAngleX, 0, 0);
     }
-
-
     public void SitSightChange(bool isSit)
     {
-        float y = isSit ? _sitSight : _idleSight;
-        transform.localPosition = new Vector3(0, y, 0.6f);
+        // 현재 진행 중인 코루틴이 있다면 중단
+        if (_crouchCoroutine != null)
+        {
+            StopCoroutine(_crouchCoroutine);
+        }
+        // 새로운 코루틴 실행
+        _crouchCoroutine = StartCoroutine(CrouchCoroutine(isSit));
     }
+
+    private IEnumerator CrouchCoroutine(bool isSit)
+    {
+        float applyCrouchPosY = isSit ? _sitSight : _idleSight;
+        float _posY = transform.localPosition.y;
+        int count = 0;
+
+        while (Mathf.Abs(_posY - applyCrouchPosY) > 0.01f) // 위치 차이가 0.01 이상일 때만
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.5f);
+            transform.localPosition = new Vector3(0, _posY, 0.6f);
+            if (count > 15)
+                break;
+            yield return null;
+        }
+        transform.localPosition = new Vector3(0, applyCrouchPosY, 0.6f);
+
+        // 코루틴이 끝나면 null로 초기화
+        _crouchCoroutine = null;
+    }
+
+
 }
