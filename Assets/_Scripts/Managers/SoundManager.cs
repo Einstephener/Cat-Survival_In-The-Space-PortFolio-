4e9902909d06f3f83de7ca78ff468e74613af862
Kwon O Tae <oteosuk@naver.com>
2024-10-31 16:34:01 +0900

    [Add] SoundManager

---
+++ Assets/_Scripts/Managers/SoundManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [Header("Audio Sources")]
+    public AudioSource musicSource;
+    public List<AudioSource> sfxSources;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayBGM(string clipName, float volume)
+    {
+        AudioClip clip = Resources.Load<AudioClip>("Sound/BGM/" + clipName);
+        if (clip != null)
+        {
+            musicSource.clip = clip;
+            musicSource.volume = volume;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string clipName, float volume)
+    {
+        AudioClip clip = Resources.Load<AudioClip>("Sound/SFX/" + clipName);
+        if (clip != null)
+        {
+            // 사용 가능한 AudioSource 찾기
+            foreach (var source in sfxSources)
+            {
+                if (!source.isPlaying)
+                {
+                    musicSource.volume = volume;
+                    source.PlayOneShot(clip);
+                    return;
+                }
+            }
+            // 만약 모든 AudioSource가 사용 중이라면, 추가적인 처리 필요 (예: 경고 메시지)
+        }
+    }
+}
